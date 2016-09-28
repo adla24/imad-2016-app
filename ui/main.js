28695a21f9e8055c3bfc7d4dd627faf433e603e5
adla24 <adityalamba_manmohan@srmuniv.edu.in>
2016-09-28 02:49:45 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,13 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter=0;
 
-var element=document.getElementById('main-text');
-
-element.innerHTML="Ne value";
-
-var img=document.getElementById('madi');
-var marginLeft= 0;
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft +'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,100);
+button.onclick = function(){
+    
+    
+    
+    
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
-
